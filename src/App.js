15af35d650591a8b57fbcb57bf2ab9d9eb6cb254
julian libor <julian.libor@gmail.com>
2021-01-31 13:22:52 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    test1: false,
+    test2: false,
+  }
+
+  toggleTest1 = () => this.setState({test1: true})
+  toggleTest2 = () => this.setState({test2: true})
+
   render() {
+    const {test1, test2} = this.state
     return (
       <div>
           <header> test</header>
+        <div>
+            {test1 && 'text2'}
+            <button onClick={this.toggleTest1}/>
+        </div>
+        <div>
+            {test2 && 'text2'}
+            <button onClick={this.toggleTest2}/>
+        </div>
       </div>
     );
   }
